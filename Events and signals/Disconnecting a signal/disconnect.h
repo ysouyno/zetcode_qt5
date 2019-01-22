ece134b8cf00c16bb97686f6adce6ea71147e26f
ysouyno <ysouyno@163.com>
2019-01-22 13:53:29 +0800

    chapter Events and signals, Disconnecting a signal

---
+++ Events and signals/Disconnecting a signal/disconnect.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QWidget>
+#include <QPushButton>
+
+class Disconnect : public QWidget
+{
+  Q_OBJECT;
+
+ public:
+  Disconnect(QWidget *parent = 0);
+
+ private slots:
+  void onClick();
+  void onCheck(int);
+
+ private:
+  QPushButton *clickBtn;
+};
---
+++ Events and signals/Disconnecting a signal/main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include "disconnect.h"
+
+int main(int argc, char *argv[])
+{
+  QApplication app(argc, argv);
+
+  Disconnect window;
+
+  window.resize(250, 150);
+  window.setWindowTitle("Disconnect");
+  window.show();
+
+  return app.exec();
+}
